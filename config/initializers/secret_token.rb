b647c4e2c8e3e633b243f773f6f3e55c547f795c
tyagbeja <tyagbeja@gmail.com>
2013-11-18 17:00:12 +0000

    'comnplete-project'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rsveep::Application.config.secret_key_base = '560365c874ea2d24bb27ed75303441d0848d62ba8bac5b6429a2a7e95158618e4e4223b02b5bcfe4b5713f483ce7b21569fe6c47636b5a1ead6a7fae79ed1310'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rsveep::Application.config.session_store :cookie_store, key: '_rsveep_session'
